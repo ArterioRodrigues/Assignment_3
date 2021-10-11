33fab826d48365f94b54d58808d76def6efdb00b
ArterioRodrigues <arterio369@gmail.com>
2021-10-11 01:47:44 -0400

    Did every thing in one go Oppsie :)

--- script.js
+++ script.js
@@ -4,35 +4,148 @@ let colorSelected;
 
 //Add a row
 function addR() {
-    alert("Clicked Add Row")
+    grid = document.getElementById("grid");
+    table_r = document.createElement("TR");
+    table_d = document.createElement("TD");
+
+    table_r.className = "table row";
+    table_d.className = "table content";
+
+    if(grid.hasChildNodes())
+    {   
+        for(let i = 0; i < document.querySelector("tr").childElementCount; i++)
+        {
+            table_r.appendChild(table_d);
+            table_d = document.createElement("TD");
+            table_d.className = "table content";
+        }
+        grid.appendChild(table_r);
+    }
+    else
+    {
+        table_r.appendChild(table_d)
+        grid.appendChild(table_r)   
+    }
+
+    numRows++;
+    nodeEvent();
 }
+
 //Add a column
-function addC() {
-    alert("Clicked Add Col")
+function addC() { 
+    grid = document.getElementById("grid");
+    table_r = document.createElement("TR");
+    table_d = document.createElement("TD");
+    table_r_items = document.getElementsByClassName("table row")
+    table_r.className = "table row";
+    table_d.className = "table content";
+
+    if(grid.hasChildNodes())
+    {
+        for(let i = 0; i < table_r_items.length; i++)
+        {
+            table_r_items[i].appendChild(table_d);
+            table_d = document.createElement("TD");
+            table_d.className = "table content";
+        }
+    }
+    else
+    {
+        table_r.appendChild(table_d);
+        grid.appendChild(table_r);  
+    }
+    numCols++;
+    nodeEvent();
 }
 
 //Remove a row
 function removeR() {
-    alert("Clicked Remove Row")
+    grid = document.getElementById("grid");
+    if(grid.hasChildNodes())
+    {
+        table_r_items = document.getElementsByClassName("table row");
+        grid.removeChild(table_r_items[table_r_items.length -1]);
+        numRows--;
+    }
 }
+
 //Remove a column
 function removeC() {
-    alert("Clicked Remove Col")
+    grid = document.getElementById("grid");
+    if(grid.hasChildNodes())
+    {
+        table_r_items = document.getElementsByClassName("table row");
+        
+    
+        for(let i = 0; i < table_r_items.length; i++)
+        {
+            table_d = table_r_items[i].childNodes;
+            table_r_items[i].removeChild(table_d[table_d.length-1]);
+        }
+        numCols--; 
+    }
 }
+
+function nodeEvent(){
+    table_d_items = document.querySelectorAll('TD')   
+    table_d_items.forEach(element => {
+        element.addEventListener("click", () => { 
+            if(colorSelected == "Red")
+                element.style.backgroundColor = 'Red' ;
+            else if(colorSelected == "Blue")
+                element.style.backgroundColor = 'blue' ;
+            else if(colorSelected == "Yellow")
+                element.style.backgroundColor = 'yellow' ;
+            else if(colorSelected == "Green")
+                element.style.backgroundColor = 'green' ;
+            else
+                element.style.backgroundColor = 'white' ;
+            ;});    
+     });
+}
+
 //sets global var for selected color
 function selected(){
     colorSelected = document.getElementById("selectedID").value;
-    console.log(colorSelected);
+    console.log(colorSelected); 
 }
 
 function fill(){
-    alert("Clicked Fill All")
+    table_d_items = document.querySelectorAll('TD')   
+    table_d_items.forEach(element => {
+        if(colorSelected == "Red")
+            element.style.backgroundColor = 'Red' ;
+        else if(colorSelected == "Blue")
+            element.style.backgroundColor = 'blue' ;
+        else if(colorSelected == "Yellow")
+            element.style.backgroundColor = 'yellow' ;
+        else if(colorSelected == "Green")
+            element.style.backgroundColor = 'green' ;
+        else
+            element.style.backgroundColor = 'white' ;
+    });
 }
 
 function clearAll(){
-    alert("Clicked Clear All")
+    table_d_items = document.querySelectorAll('TD')   
+    table_d_items.forEach(element => {
+        element.style.backgroundColor = 'white' ;
+    });
 }
 
 function fillU(){
-    alert("Clicked Fill All Uncolored")
+    table_d_items = document.querySelectorAll('TD')   
+    table_d_items.forEach(element => {
+        if(element.style.backgroundColor === 'white' || element.style.backgroundColor === '')
+        {
+            if(colorSelected == "Red")
+                element.style.backgroundColor = 'Red' ;
+            else if(colorSelected == "Blue")
+                element.style.backgroundColor = 'blue' ;
+            else if(colorSelected == "Yellow")
+                element.style.backgroundColor = 'yellow' ;
+            else if(colorSelected == "Green")
+                element.style.backgroundColor = 'green' ;
+        }
+    });
 }
